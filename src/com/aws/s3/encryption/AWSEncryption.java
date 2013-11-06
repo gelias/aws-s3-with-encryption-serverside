7017e06bc025524eb5e290581433d4417529f879
Guilherme Elias <guilherme.elias@gmail.com>
2013-11-06 21:47:38 -0200

    criado metodo para obter foto da AWS

--- src/com/aws/s3/encryption/AWSEncryption.java
+++ src/com/aws/s3/encryption/AWSEncryption.java
@@ -3,13 +3,15 @@ package com.aws.s3.encryption;
 
 public class AWSEncryption {
 
+	private static final String LOCAL_FILE = "/tmp/photo";
 	private static final String FILE_KEY = "gi-joe";
 	private static final String BUCKET_NAME = "20131103";
 	private static final String FILE_TO_UPLOAD = "./gi-joe.jpg";
 
 	public static void main(String[] args) {
-		AwsS3ServerSideEncryption s3Client = new AwsS3ServerSideEncryption("access_key", "secret_key");
+		AwsS3ServerSideEncryption s3Client = new AwsS3ServerSideEncryption("AKIAJHLQSOW6PHFQ6MFA", "Ws2bAjooigvBTSqWjtd1iBMxMHzSp1HAaFJMAPSb");
 		s3Client.uploadFile(FILE_TO_UPLOAD).toFolder(BUCKET_NAME, FILE_KEY).usingServerSideEncryption().go();
+		s3Client.getPhoto(BUCKET_NAME, FILE_KEY, LOCAL_FILE);
 	}
 
 }
--- src/com/aws/s3/encryption/AwsS3ServerSideEncryption.java
+++ src/com/aws/s3/encryption/AwsS3ServerSideEncryption.java
@@ -1,18 +1,25 @@
 package com.aws.s3.encryption;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
 
 public class AwsS3ServerSideEncryption {
 
 	private File imageToUpload;
 	private PutObjectRequest putRequest;
 	private final BasicAWSCredentials awsCredentials;
+	private AmazonS3Client s3;
 
 	public AwsS3ServerSideEncryption(String accessKey, String secretKey) {
 		this.awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
@@ -45,9 +52,28 @@ public class AwsS3ServerSideEncryption {
 	}
 
 	private PutObjectResult dispatchObject(PutObjectRequest putRequest) {
-		AmazonS3Client s3 = new AmazonS3Client(awsCredentials);
+		this.s3 = new AmazonS3Client(awsCredentials);
 		PutObjectResult response = s3.putObject(putRequest);
 		return response;
+	}
+
+	public void getPhoto(String bucket, String fileKey, String localFile) {
+		S3Object object = s3.getObject(new GetObjectRequest(bucket, fileKey));
+		InputStream objectData = object.getObjectContent();
+		try {
+			OutputStream outputStream = new FileOutputStream(new File(localFile));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			while ((read = objectData.read(bytes)) != -1) {
+				outputStream.write(bytes, 0, read);
+			}
+
+			System.out.println("Done!");
+			objectData.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
